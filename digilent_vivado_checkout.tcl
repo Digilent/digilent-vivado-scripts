cd6de68d8ed09275d901e57b408b6cf3e9ed8eab
ArtVVB <arthur.brown@digilent.com>
2018-08-09 14:56:50 -0700

    improve checkout logging

--- digilent_vivado_checkout.tcl
+++ digilent_vivado_checkout.tcl
@@ -6,64 +6,63 @@ set vivado_version [lindex $argv 2]
 set vivado_year [lindex [split $vivado_version "."] 0]
 set proj_name [file rootname [file tail $xpr_path]]
 
-puts "INFO: Creating new project \"$proj_name\" in [file dirname $repo_path]/proj"
+puts "INFO: Creating new project \"$proj_name\" in [file dirname $xpr_path]"
 
 # Create project
 create_project $proj_name [file dirname $xpr_path]
 
 # Capture board information for the project
+puts "INFO: Capturing board information from $repo_path/project_info.tcl"
 source $repo_path/project_info.tcl
-
-# Set project properties (using proc declared in project_info.tcl)
 set_digilent_project_properties $proj_name
 set obj [get_projects $proj_name]
 set part_name [get_property "part" $obj]
 
 # Uncomment the following 3 lines to greatly increase build speed while working with IP cores (and/or block diagrams)
+puts "INFO: Configuring project IP handling properties"
 set_property "corecontainer.enable" "0" $obj
 set_property "ip_cache_permissions" "read write" $obj
 set_property "ip_output_repo" "[file normalize "$repo_path/repo/cache"]" $obj
 
 # Create 'sources_1' fileset (if not found)
 if {[string equal [get_filesets -quiet sources_1] ""]} {
+	puts "INFO: Creating sources_1 fileset"
     create_fileset -srcset sources_1
 }
 
 # Create 'constrs_1' fileset (if not found)
 if {[string equal [get_filesets -quiet constrs_1] ""]} {
+	puts "INFO: Creating constrs_1 fileset"
     create_fileset -constrset constrs_1
 }
 
 # Set IP repository paths
+puts "INFO: Setting IP repository paths"
 set obj [get_filesets sources_1]
 set_property "ip_repo_paths" "[file normalize $repo_path/repo]" $obj
 
 # Refresh IP Repositories
+puts "INFO: Refreshing IP repositories"
 update_ip_catalog -rebuild
 
 # Add hardware description language sources
+puts "INFO: Adding HDL sources"
 add_files -quiet -norecurse $repo_path/src/hdl
 
 # Add IPs
 # TODO: handle IP core-container files
+puts "INFO: Adding XCI IP sources"
 add_files -quiet [glob -nocomplain $repo_path/src/ip/*/*.xci]
 
 # Add constraints
+puts "INFO: Adding constraints"
 add_files -quiet -norecurse -fileset constrs_1 $repo_path/src/constraints
 
-														 
-											
- 
-																   
-													  
- 
-																	  
-														 
- 
-
 # Recreate block design
+# TODO: handle bd filesets
 set bd_files [glob -nocomplain "$repo_path/src/bd/*.tcl"]
-if {[llength $bd_files] == 1} {
+if {[llength $bd_files] >= 1} {
+	puts "INFO: Rebuilding block design from script"
 	# Create local source directory for bd
 	if {[file exist "[file rootname $xpr_path].srcs"] == 0} {
 		file mkdir "[file rootname $xpr_path].srcs"
@@ -84,15 +83,20 @@ if {[llength $bd_files] == 1} {
 
     set obj [get_filesets sources_1]
     set_property "top" "${design_name}_wrapper" $obj
+} else {
+	puts "INFO: no block design TCL script detected"
+	# TODO: handle adding BD sources from complete fileset
 }
 
 # Create 'synth_1' run (if not found)
 if {[string equal [get_runs -quiet synth_1] ""]} {
+	puts "INFO: Creating synth_1 run"
     create_run -name synth_1 -part $part_name -flow {Vivado Synthesis $vivado_year} -strategy "Vivado Synthesis Defaults" -constrset constrs_1
 } else {
     set_property strategy "Vivado Synthesis Defaults" [get_runs synth_1]
     set_property flow "Vivado Synthesis $vivado_year" [get_runs synth_1]
 }
+puts "INFO: Configuring synth_1 run"
 set obj [get_runs synth_1]
 set_property "part" $part_name $obj
 set_property "steps.synth_design.args.flatten_hierarchy" "none" $obj
@@ -100,15 +104,18 @@ set_property "steps.synth_design.args.directive" "RuntimeOptimized" $obj
 set_property "steps.synth_design.args.fsm_extraction" "off" $obj
 
 # Set the current synth run
+puts "INFO: Setting current synthesis run"
 current_run -synthesis [get_runs synth_1]
 
 # Create 'impl_1' run (if not found)
 if {[string equal [get_runs -quiet impl_1] ""]} {
+	puts "INFO: Creating impl_1 run"
     create_run -name impl_1 -part $part_name -flow {Vivado Implementation $vivado_year} -strategy "Vivado Implementation Defaults" -constrset constrs_1 -parent_run synth_1
 } else {
     set_property strategy "Vivado Implementation Defaults" [get_runs impl_1]
     set_property flow "Vivado Implementation $vivado_year" [get_runs impl_1]
 }
+puts "INFO: Configuring impl_1 run"
 set obj [get_runs impl_1]
 set_property "part" $part_name $obj
 set_property "steps.opt_design.args.directive" "RuntimeOptimized" $obj
@@ -116,6 +123,8 @@ set_property "steps.place_design.args.directive" "RuntimeOptimized" $obj
 set_property "steps.route_design.args.directive" "RuntimeOptimized" $obj
 
 # Set the current impl run
+puts "INFO: Setting current implementation run"
 current_run -implementation [get_runs impl_1]
 
 puts "INFO: Project created: [file tail $proj_name]"
+puts "INFO: Exiting Vivado"
